ec692ae17cd0e1497714701b221fefa859e414e4
Ian Cook Westgate <iwestgate@hotmail.com>
2019-04-26 13:53:46 -0700

    leave infinite scroll alone for now and begin refactoring navbar for state.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,40 +7,52 @@ import twitter from '../assets/images/twitter.png';
 import fbook from '../assets/images/fbook.png';
 import magGlass from '../assets/images/magGlass.png';
 
-function Navbar(){
-  return (
-    <div className='bg-grn'>
-      <div className='navbar'>
-        <div className='topbar'>
-          <img id="m-icon" src={mIcon}></img>
-          <div className="rowify">
-            <Link id="sign-in" to="/">Sign in</Link>
-            <Link id="curve-border" to="/">Get started</Link>
-          </div>
-        </div>
-        <div className='secondary-bar'>
-          <div className="imgManip">
-            <Link to ="/"><img id="logo" src={hNoon}></img></Link>
-          </div>
-          <div className="split">
-            <div className="leftside">
-              <a href="#">LATEST</a>
-              <a href="#">TOP</a>
-              <a href="#">2.0</a>
-              <a id="bord-right" href="#">PODCAST</a>
-              <a href="#">GET PUBLISHED</a>
+class Navbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      activeClass: true,
+      stageVisibleOnPage: true
+    };
+    this.disappear = this.disappear.bind(this);
+  }
+
+  render() {
+    return (
+      <div className='bg-grn'>
+        <div className='navbar'>
+          <div className='topbar'>
+            <img id="m-icon" src={mIcon}></img>
+            <div className="rowify">
+              <Link id="sign-in" to="/">Sign in</Link>
+              <Link id="curve-border" to="/">Get started</Link>
             </div>
-            <div className="icons">
-              <img src={magGlass}></img>
-              <img src={twitter}></img>
-              <img src={fbook}></img>
-              <a id="follow" href="#">Follow</a>
+          </div>
+          <div className='secondary-bar'>
+            <div className="imgManip">
+              <Link to ="/"><img id="logo" src={hNoon}></img></Link>
+            </div>
+            <div className="split">
+              <div className="leftside">
+                <a href="#">LATEST</a>
+                <a href="#">TOP</a>
+                <a href="#">2.0</a>
+                <a id="bord-right" href="#">PODCAST</a>
+                <a href="#">GET PUBLISHED</a>
+              </div>
+              <div className="icons">
+                <img src={magGlass}></img>
+                <img src={twitter}></img>
+                <img src={fbook}></img>
+                <a id="follow" href="#">Follow</a>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default Navbar;
