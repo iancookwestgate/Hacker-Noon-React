d0dfa83bdbf26dcadce5dc17082c34a8ae5abaac
Ian Cook Westgate <iwestgate@hotmail.com>
2019-04-26 14:17:56 -0700

    add in a test section to see if logo onClick is working.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,37 +28,42 @@ class Navbar extends React.Component {
 
   render() {
     return (
-      <div className='bg-grn'>
-        <div className='navbar'>
-          <div className='topbar'>
-            <img id="m-icon" src={mIcon}></img>
-            <div className="rowify">
-              <Link id="sign-in" to="/">Sign in</Link>
-              <Link id="curve-border" to="/">Get started</Link>
-            </div>
-          </div>
-          <div className='secondary-bar'>
-            <div className="imgManip">
-              <Link to ="/" onClick={()=>this.disappear()}><img id="logo" src={hNoon}></img></Link>
-            </div>
-            <div className="split">
-              <div className="leftside">
-                <a href="#">LATEST</a>
-                <a href="#">TOP</a>
-                <a href="#">2.0</a>
-                <a id="bord-right" href="#">PODCAST</a>
-                <a href="#">GET PUBLISHED</a>
+      <div>
+        <div className='bg-grn'>
+          <div className='navbar'>
+            <div className='topbar'>
+              <img id="m-icon" src={mIcon}></img>
+              <div className="rowify">
+                <Link id="sign-in" to="/">Sign in</Link>
+                <Link id="curve-border" to="/">Get started</Link>
               </div>
-              <div className="icons">
-                <img src={magGlass}></img>
-                <img src={twitter}></img>
-                <img src={fbook}></img>
-                <a id="follow" href="#">Follow</a>
+            </div>
+            <div className='secondary-bar'>
+              <div className="imgManip">
+                <Link to ="/" onClick={()=>this.disappear()}><img id="logo" src={hNoon}></img></Link>
+              </div>
+              <div className="split">
+                <div className="leftside">
+                  <a href="#">LATEST</a>
+                  <a href="#">TOP</a>
+                  <a href="#">2.0</a>
+                  <a id="bord-right" href="#">PODCAST</a>
+                  <a href="#">GET PUBLISHED</a>
+                </div>
+                <div className="icons">
+                  <img src={magGlass}></img>
+                  <img src={twitter}></img>
+                  <img src={fbook}></img>
+                  <a id="follow" href="#">Follow</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
+      <div className={true===this.state.stageVisibleOnPage? "hide" : "show"}>
+        <h1>You found me!</h1>
       </div>
+    </div>
     );
   }
 
