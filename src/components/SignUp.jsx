5d4bfb73585a774bd67a8047e9ea0c30db27c005
Ian Cook Westgate <iwestgate@hotmail.com>
2019-04-26 11:13:45 -0700

    finish properly styling the button of SignUo.

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import computer from '../assets/images/hacker-comp.png';
+import { Link } from 'react-router-dom';
 import '../scss/styles.scss';
 
 function SignUp(){
@@ -7,7 +8,7 @@ function SignUp(){
     <div className="bg-grn signUp">
       <img id="compu" src={computer}></img>
       <div className="signup-sep">
-        <a href="#" id="newsletter" type="submit">Signup for the Hacker Noon Newsletter</a>
+        <Link to="/" id="newsletter">Signup for the Hacker Noon Newsletter</Link>
       </div>
     </div>
   );
